c83c03a70a54ababf4c7e1f7275927d906aac3f6
William Koller <williamkoller404@gmail.com>
2023-01-05 15:37:10 -0300

    chore: initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const logger = new Logger('Main');
+  const port = 3002;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port, () => logger.log(`Server listening at: ${port}`));
+}
+bootstrap();
