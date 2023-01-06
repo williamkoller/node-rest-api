0ca9fd9138c0081192415d57ed06188fc702878f
William Koller <williamkoller404@gmail.com>
2023-01-06 13:03:39 -0300

    feat: :sparkles: add new validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
   const config = app.get<ConfigService>(ConfigService);
 
